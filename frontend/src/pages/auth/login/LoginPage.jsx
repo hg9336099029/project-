87497a6877c9a78506f8c45cf20e176aa079a1c3
harsh <hg3172003@gmail.com>
2024-12-10 01:00:19 +0530

    23-up

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
-import XSvg from "../../../components/svgs/X";
-
 import { MdOutlineMail } from "react-icons/md";
 import { MdPassword } from "react-icons/md";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
@@ -58,13 +55,9 @@ const LoginPage = () => {
 
   return (
     <div className="max-w-screen-xl mx-auto flex h-screen">
-      <div className="flex-1 hidden lg:flex items-center  justify-center">
-        <XSvg className="lg:w-2/3 fill-white" />
-      </div>
-      <div className="flex-1 flex flex-col justify-center items-center">
-        <form className="flex gap-4 flex-col" onSubmit={handleSubmit}>
-          <XSvg className="w-24 lg:hidden fill-white" />
-          <h1 className="text-4xl font-extrabold text-white">{"Let's"} go.</h1>
+      <div className="flex-1 flex  flex-col justify-center items-center">
+        <form className="flex gap-6 flex-col" onSubmit={handleSubmit}>
+           <h1 className=" flex-auto text-6xl mx-1 my-10 font-extrabold text-yellow-500"> SOCIAL go.</h1>
           <label className="input input-bordered rounded flex items-center gap-2">
             <MdOutlineMail />
             <input
